93dd56d004472f1b52806b32041656d893778b4b
Evgene Popolamov <popolamov908@gmail.com>
2020-12-10 23:07:33 +0300

    feat: create sidebar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import listSvg from "./assets/icons/list.svg";
 
 function App() {
-  return <div className="App">123</div>;
+  return (
+    <div className="todo">
+      <div className="todo__sidebar">
+        <ul className="todo__list">
+          <li>
+            <i>
+              <img src={listSvg} alt="list-icon" />
+            </i>
+            <span>Все задачи</span>
+          </li>
+        </ul>
+      </div>
+      <div className="todo__tasts"></div>
+    </div>
+  );
 }
 
 export default App;
