7a0331ccccb163f0a1facba2fe9b642db3c392a2
Evgene Popolamov <popolamov908@gmail.com>
2020-12-11 20:03:46 +0300

    feat: add hooks

---
+++ src/Components/List/List.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import classNames from "classnames";
+import "./List.scss";
+
+const List = ({ items, isRemovable, onClick }) => {
+  return (
+    <ul onClick={onClick} className="list">
+      {items.map((item, index) => {
+        return (
+          <li
+            key={index}
+            className={classNames(item.className, { active: item.active })}
+          >
+            <i>
+              {item.icon ? (
+                item.icon
+              ) : (
+                <i className={`badge badge--${item.color}`}></i>
+              )}
+            </i>
+            <span>{item.name}</span>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default List;
